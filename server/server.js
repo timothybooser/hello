77859c344cee4dd4076390a23861388108985c80
Timothy Booser <tbooser@utexas.edu>
2017-03-19 20:41:47 -0500

    Hooked up

--- server/server.js
+++ server/server.js
@@ -1,41 +1,19 @@
-// const http = require('http');
-// const port = 3000;
-
-// const requestHandler = (request, response) => {
-// 	console.log(request.url);
-// 	response.end('Hello');
-// }
-
-// const server = http.createServer(requestHandler); 
-
-// server.listen(port, err => {
-// 	if(err){
-// 		return console.log('There was an error', err)
-// 	}
-
-// 	console.log('Server is listening on port ' + port)
-// })
-
-
 var express = require('express');
 var Path = require('path');
 var routes = express.Router();
-
+ 
 //Route to index.html
-var assetFolder = Path.resolve(__dirname, '../src/client/');
+var assetFolder = Path.resolve(__dirname, '../');
   routes.use(express.static(assetFolder));
 
 if (process.env.NODE_ENV !== 'test') {
   routes.get('/*', function(req, res){
-    res.sendFile( assetFolder + '/index.html' )
+    res.sendFile(assetFolder + 'index.html')
   })
-
 
 var app = express();
   app.use( require('body-parser').json() )
   app.use('/', routes);
-
-
 
 module.exports = app;
  
