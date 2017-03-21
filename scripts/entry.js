713bd58bd6b92f0601740c38cdb5ba18939fc6d9
Timothy Booser <tbooser@utexas.edu>
2017-03-20 22:47:14 -0500

    SASS set up

--- scripts/entry.js
+++ scripts/entry.js
@@ -1,1 +1,11 @@
-document.write("Entry.js");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/main.scss';
+
+
+const element = <h1>Hello, world</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('main')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,9 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            {test: /\.css$/, loader: "style!css" },
+            {test: /\.scss$/, loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]},
+            {test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015']}}
         ]
     }
 };
